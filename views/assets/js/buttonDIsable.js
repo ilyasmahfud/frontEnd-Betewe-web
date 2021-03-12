0e61b1c0cde195fcd58a634137f226878c7bc6b4
root <root@MSI.localdomain>
2021-03-12 17:37:43 +0700

    fix alert and disable/enable button

--- views/assets/js/buttonDIsable.js
+++ views/assets/js/buttonDIsable.js
@@ -3,38 +3,48 @@
 $("#pertanyaan").on("keyup",function () {
     let pertanyaan = $("#pertanyaan").val();
     if (pertanyaan == "" ) {
-        $("#btnSubmited").css("background-color","#BCBCBC")
-        $("#btnSubmited").attr("disabled", true);
-        return false
+            $("#btnSubmited").css("background-color","#BCBCBC")
+            $("#btnSubmited").attr("disabled", true);
+            return false
     } 
     else {
-        $("#fakultas").on("keyup",function () {
-            let pertanyaan = $("#fakultas").val();
+        $("#deskripsiPertanyaan").on("keyup",function () {
+            let pertanyaan = $("#deskripsiPertanyaan").val();
             if (pertanyaan == "" ) {
                 $("#btnSubmited").css("background-color","#BCBCBC")
                 $("#btnSubmited").attr("disabled", true);
                 return false
             } 
             else {
-                $("#programStudi").on("keyup",function () {
-                    let pertanyaan = $("#programStudi").val();
+                $("#fakultas").on("keyup",function () {
+                    let pertanyaan = $("#fakultas").val();
                     if (pertanyaan == "" ) {
                         $("#btnSubmited").css("background-color","#BCBCBC")
                         $("#btnSubmited").attr("disabled", true);
                         return false
                     } 
                     else {
-                        $("#matkul").on("keyup",function () {
-                            let pertanyaan = $("#matkul").val();
+                        $("#programStudi").on("keyup",function () {
+                            let pertanyaan = $("#programStudi").val();
                             if (pertanyaan == "" ) {
                                 $("#btnSubmited").css("background-color","#BCBCBC")
                                 $("#btnSubmited").attr("disabled", true);
                                 return false
-                            } 
-                            else {
-                                $("#btnSubmited").css("background-color","#2C448C")
-                                $("#btnSubmited").attr("disabled", false);
-                                return true
+                                } 
+                                else {
+                                    $("#matkul").on("keyup", function(){
+                                        let matkul = $("#matkul").val()
+                                        if (matkul == "") {
+                                            $("#btnSubmited").css("background-color","#BCBCBC")
+                                            $("#btnSubmited").attr("disabled", true);
+                                            return false
+                                        }
+                                        else {
+                                            $("#btnSubmited").css("background-color","#2C448C")
+                                            $("#btnSubmited").attr("disabled", false);
+                                            return true
+                                        }
+                                    })
                             }
                         });
                     }
@@ -43,8 +53,5 @@ $("#pertanyaan").on("keyup",function () {
         });
     }
 });
-
-
-
 
 
