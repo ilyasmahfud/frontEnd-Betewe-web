51e2961693fc181873e1badcda6d371c5769b997
sirius146 <dwiatika479@yahoo.com>
2021-03-13 17:00:08 +0700

    Perbaiki halaman beranda

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,42 +31,42 @@ jQuery(document).ready(function(){
     })
 
 
-    //animasi fadein
-    // Trigger CSS animations on scroll.
-// Detailed explanation can be found at http://www.bram.us/2013/11/20/scroll-animations/
+//     //animasi fadein
+//     // Trigger CSS animations on scroll.
+// // Detailed explanation can be found at http://www.bram.us/2013/11/20/scroll-animations/
 
-// Looking for a version that also reverses the animation when
-// elements scroll below the fold again?
-// --> Check https://codepen.io/bramus/pen/vKpjNP
+// // Looking for a version that also reverses the animation when
+// // elements scroll below the fold again?
+// // --> Check https://codepen.io/bramus/pen/vKpjNP
 
-jQuery(function($) {
+// jQuery(function($) {
   
-  // Function which adds the 'animated' class to any '.animatable' in view
-  var doAnimations = function() {
+//   // Function which adds the 'animated' class to any '.animatable' in view
+//   var doAnimations = function() {
     
-    // Calc current offset and get all animatables
-    var offset = $(window).scrollTop() + $(window).height(),
-        $animatables = $('.animatable');
+//     // Calc current offset and get all animatables
+//     var offset = $(window).scrollTop() + $(window).height(),
+//         $animatables = $('.animatable');
     
-    // Unbind scroll handler if we have no animatables
-    if ($animatables.length == 0) {
-      $(window).off('scroll', doAnimations);
-    }
+//     // Unbind scroll handler if we have no animatables
+//     if ($animatables.length == 0) {
+//       $(window).off('scroll', doAnimations);
+//     }
     
-    // Check all animatables and animate them if necessary
-		$animatables.each(function(i) {
-       var $animatable = $(this);
-			if (($animatable.offset().top + $animatable.height() - 20) < offset) {
-        $animatable.removeClass('animatable').addClass('animated');
-			}
-    });
+//     // Check all animatables and animate them if necessary
+// 		$animatables.each(function(i) {
+//        var $animatable = $(this);
+// 			if (($animatable.offset().top + $animatable.height() - 20) < offset) {
+//         $animatable.removeClass('animatable').addClass('animated');
+// 			}
+//     });
 
-	};
+// 	};
   
-  // Hook doAnimations on scroll, and trigger a scroll
-	$(window).on('scroll', doAnimations);
-  $(window).trigger('scroll');
+//   // Hook doAnimations on scroll, and trigger a scroll
+// 	$(window).on('scroll', doAnimations);
+//   $(window).trigger('scroll');
 
-});
+// });
 
 
